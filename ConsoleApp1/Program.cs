d717db76d5ba70236e68fb8e5fc3655a1232bc18
3110476 <3110476@TE-GDA-D-D26F02>
2022-03-11 15:16:16 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿Random r = new Random();
+
+int los = r.Next(1, 10);
+int zgadnij = 0;
+bool petla = false;
+
+Console.WriteLine("Wybieram liczbe od 1 do 10");
+
+while (!petla)
+{
+    Console.Write("zgadnij: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out zgadnij))
+    {
+        Console.WriteLine("wpisz cyfre");
+        continue;
+    }
+
+    if (zgadnij < los)
+    {
+        Console.WriteLine("wyzej");
+    }
+    else if (zgadnij > los)
+    {
+        Console.WriteLine("Nizej");
+    }
+    else
+    {
+        petla = true;
+        Console.WriteLine("Brawo zgadłeś!");
+    }
+}
